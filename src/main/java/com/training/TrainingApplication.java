caaa5f83b2807ff3ea78eb77f17c5cd96db4820b
jacob <jacobryuu1225@gmail.com>
2022-02-19 23:08:51 +0900

    add log4j

--- src/main/java/com/training/TrainingApplication.java
+++ src/main/java/com/training/TrainingApplication.java
@@ -2,12 +2,17 @@ package com.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableOpenApi
+@ComponentScan
 public class TrainingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingApplication.class, args);
+    }
 
 }
